aa08220b393768e2cc97b65e526b4ff51e384c90
Lucas Lacerda <lucaslacerdajob@gmail.com>
2022-04-09 18:09:57 -0300

    Initial Commit

---
+++ AluraViagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AluraViagens
+//
+//  Created by Lucas Lacerda on 09/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
